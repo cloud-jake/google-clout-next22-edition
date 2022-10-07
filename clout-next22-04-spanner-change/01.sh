f36a853d432382c45a0788b162f648a69eab53ec
cloud-jake <jake.holmquist@kinandcarta.com>
2022-10-06 22:47:28 -0400

    spanner

--- clout-next22-04-spanner-change/01.sh
+++ clout-next22-04-spanner-change/01.sh
@@ -33,3 +33,8 @@ gcloud config set project $PROJECT
 # Add Data
 gcloud spanner databases execute-sql $SOURCE_DATABASE --instance=$SOURCE_INSTANCE --sql="INSERT INTO orders(OrderID,CustomerID,OrderDate,Price,ProductID) VALUES(123,456,'2022-04-26',99,789);"
 
+gcloud spanner databases execute-sql $SOURCE_DATABASE --instance=$SOURCE_INSTANCE --sql="INSERT INTO orders(OrderID,CustomerID,OrderDate,Price,ProductID) VALUES(1231,456,'2022-04-26',99,789);"
+
+gcloud spanner databases execute-sql $SOURCE_DATABASE --instance=$SOURCE_INSTANCE --sql="INSERT INTO orders(OrderID,CustomerID,OrderDate,Price,ProductID) VALUES(1232,456,'2022-04-26',99,789);"
+
+gcloud spanner databases execute-sql $SOURCE_DATABASE --instance=$SOURCE_INSTANCE --sql="INSERT INTO orders(OrderID,CustomerID,OrderDate,Price,ProductID) VALUES(1233,456,'2022-04-26',99,789);"
